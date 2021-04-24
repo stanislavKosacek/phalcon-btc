42cfed303369da326f485c9c96cacd05bc392d05
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-25 00:25:13 +0200

    auto versing js

--- application/app/controllers/BaseController.php
+++ application/app/controllers/BaseController.php
@@ -2,12 +2,20 @@
 
 use Phalcon\Mvc\Controller;
 use Phalcon\Tag;
-
+use Phalcon\Assets\Asset\Js;
 class BaseController extends Controller
 {
+
 	public function onConstruct()
 	{
-		$this->assets->addJs('front/dist/bundle.js');
+		$this->assets->addAsset(new Js(
+			'front/dist/bundle.js',
+			true,
+			true,
+			[],
+			null,
+			true
+		));
 		Tag::setTitle('Bitcoin price');
 	}
 }
